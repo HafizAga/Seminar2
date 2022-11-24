1c783a43799fe58f2d9398fc7dda798f7fdbfa5f
HafiAga <ulguiorra2@gmail.com>
2022-11-24 21:23:57 +0300

    Дз13.

--- Домашнее задание 13/Program.cs
+++ Домашнее задание 13/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число: ");
+string a = Console.ReadLine();
+if(Convert.ToInt32(a) < 100) {
+    Console.WriteLine("Третьей цифры нет");
+}
+
+else {
+    char[] arr = new char[a.Length];
+        for (int i = 0; i < a.Length; i++) {
+        arr[i] = a[i];    
+        }
+    Console.WriteLine("Третья цифра: " + arr[2]);
+}
+
