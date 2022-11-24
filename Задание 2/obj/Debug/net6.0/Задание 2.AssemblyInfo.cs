4e999d08da490ea77a44afa044bdf124f7ccd074
HafiAga <ulguiorra2@gmail.com>
2022-11-24 21:21:18 +0300

    Дз13

--- Задание 2/obj/Debug/net6.0/Задание 2.AssemblyInfo.cs
+++ Задание 2/obj/Debug/net6.0/Задание 2.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Домашнее задание 13")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Задание 2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Домашнее задание 13")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Домашнее задание 13")]
+[assembly: System.Reflection.AssemblyProductAttribute("Задание 2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Задание 2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
